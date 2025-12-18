dd11d60361684d9c5fb16880aa53693d424221dd
slfrisby <saskia.frisby@mrc-cbu.cam.ac.uk>
2025-12-18 11:25:54 +0000

    take 2 - updated rerun functionality!

--- src/rerun_on_chtc.m
+++ src/rerun_on_chtc.m
@@ -17,8 +17,14 @@ missingIndex = false(size(queueInput,1),1);
 
 % for every folder in queue_input.csv
 for i = 1:size(queueInput,1)
-    % if the folder does not contain results.mat
-    if ~exist([root,'\',queueInput{i,1},'\results.mat'])
+    % print the folder name
+    disp(queueInput{i,1})
+    % attempt to load the file
+    try load([root,'/',queueInput{i,1},'/results.mat'])
+        % if it fails
+    catch
+        % print an indication
+        disp('- requires rerun')
         % mark the file as missing
         missingIndex(i) = true;
     end
